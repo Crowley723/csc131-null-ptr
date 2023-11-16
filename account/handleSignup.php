10350ad17994fef24042e1cd8f91af8e64a2a096
Crowley723 <littlehill723@gmail.com>
2023-11-16 12:45:00 -0800

    completed signup handling

--- account/handleSignup.php
+++ account/handleSignup.php
@@ -1,20 +1,37 @@
 <?php
 $DBhostname = getenv("SQLHOSTNAME");
-$usersDB = getenv("CSC131USERSDB");
-$DBusername = getenv("CSC131USERSDBUSER");
-$DBpassword = getenv("CSC131USERSDBPASS");
+$usersDB = getenv("CSC131USERDBNAME");
+$DBusername = getenv("CSC131USERDBUSER");
+$DBpassword = getenv("CSC131USERDBPASS");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve form data
-    $fullName = htmlspecialchars($_POST["username"], ENT_QUOTES, 'UTF-8');
+    $fullName = htmlspecialchars($_POST["firstName"], ENT_QUOTES, 'UTF-8');
     $email = $_POST["email"];
-    $password = $_POST["password"];
+    $password = $_POST["password1"];
     $studentID = $_POST["studentID"];
 
-    if (empty($username) || empty($email) || empty($password) || empty($studentID)) {
-        echo "All fields are required.";
+    if (empty($fullName)) {
+        echo "Full Name is required.";
         exit;
     }
+    
+    if (empty($email)) {
+        echo "Email is required.";
+        exit;
+    }
+    
+    if (empty($password)) {
+        echo "Password is required.";
+        exit;
+    }
+    
+    if (empty($studentID)) {
+        echo "Student ID is required.";
+        exit;
+    }
+
+    
     if(!validateName($fullName)){
         echo "Invalid Name.";
         exit();
@@ -23,8 +40,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Invalid Email.";
         exit();
     }
-    if(!validatePassword($password)){
+    if(!passwordMatchesPattern($password)){
         echo "Invalid Password.";
+        echo $password;
         exit();
     }
     if(!validateStudentID($studentID)){
@@ -37,10 +55,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
 
 
-    $databaseConnection = new mysqli($DBhostname, $DBusername, $DBpassword, $usersDB);
+    $databaseConnection = mysqli_connect($DBhostname, $DBusername, $DBpassword, $usersDB);
 
-    if ($databaseConnection->connect_error) {
-        die("Connection failed: " . $databaseConnection->connect_error);
+    if (!$databaseConnection) {
+        echo "Error: Unable to connect to MySQL." . PHP_EOL;
+        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }else{
+        echo 'Connected to DB';
     }
     $bcryptOptions = [ 
         'cost' => 12, 
@@ -48,8 +71,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       
     $hashedPassword = password_hash($password, PASSWORD_BCRYPT, $bcryptOptions); 
     //prepared sql statement
-    $insertUserQuery = $databaseConnection->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
-    $insertUserQuery->bind_param("sss", $username, $email, $hashedPassword);
+    $insertUserQuery = mysqli_prepare($databaseConnection, "INSERT INTO csc131.Users (`Full Name`, `Email`, `Hashed Password`, `StudentID`) VALUES (?, ?, ?, ?)");
+    mysqli_stmt_bind_param($insertUserQuery, "ssss", $fullName, $email, $hashedPassword, $studentID);
 
     if ($insertUserQuery->execute() === TRUE) {
         echo "Signup successful!";
@@ -83,9 +106,9 @@ function validateName($name) {
         return false; // Invalid email
     }
 }
-function validatePassword($password) {
+function passwordMatchesPattern($password) {
     // Define a regular expression for basic email validation
-    $passwordRegex = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/";
+    $passwordRegex = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/";
 
     // Use the preg_match function to test the email against the regular expression
     if (preg_match($passwordRegex, $password)) {
