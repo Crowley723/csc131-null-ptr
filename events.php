45c272872b39c9fbe39fec9db8fec9a232cd1773
Nick J <w1868444@apps.losrios.edu>
2023-11-15 20:31:26 -0800

    created event card for incoming event

--- events.php
+++ events.php
@@ -1,9 +1,102 @@
 <!DOCTYPE html>
 <html>
 <style>
+    :root {
+  --rwgr: #043124;
+  --ssgr: #004e38;
+  --stgr: #008554;
+  --spgr: #55ba47;
+  --gvgr: #e6ea85;
+  --hgo: #e6b711;
+  --hgob: #fac606;
+  --hgobmd: #fcdc69;
+  --hgoblt: #fde89b;
+  --wgo: #dad490;
+  --wgomd: #e8e5bc;
+  --wgolt: #f2f0d9;
+  --brz: #cfb668;
+  --brzdk: #524829;
+  --white: #fff;
+  --graymin: #ddd;
+  --graylt: #aaa;
+  --graymd: #777;
+  --graydk: #333;
+  --black: #000;
+  --rwgr80a: rgba(4, 49, 36, 0.8);
+  --rwgr60a: rgba(4, 49, 36, 0.6);
+  --rwgr40a: rgba(4, 49, 36, 0.4);
+  --rwgr20a: rgba(4, 49, 36, 0.2);
+  --rwgr00a: rgba(4, 49, 36, 0);
+  --ssgr80a: rgba(0, 78, 56, 0.8);
+  --ssgr60a: rgba(0, 78, 56, 0.6);
+  --ssgr40a: rgba(0, 78, 56, 0.4);
+  --ssgr20a: rgba(0, 78, 56, 0.2);
+  --ssgr00a: rgba(0, 78, 56, 0);
+  --stgr80a: rgba(0, 133, 84, 0.8);
+  --stgr60a: rgba(0, 133, 84, 0.6);
+  --stgr40a: rgba(0, 133, 84, 0.4);
+  --stgr20a: rgba(0, 133, 84, 0.2);
+  --stgr00a: rgba(0, 133, 84, 0);
+  --spgr80a: rgba(0, 133, 84, 0.8);
+  --spgr60a: rgba(0, 133, 84, 0.6);
+  --spgr40a: rgba(0, 133, 84, 0.4);
+  --spgr20a: rgba(0, 133, 84, 0.2);
+  --spgr00a: rgba(0, 133, 84, 0);
+  --hgob80a: rgba(250, 198, 6, 0.8);
+  --hgob60a: rgba(250, 198, 6, 0.6);
+  --hgob40a: rgba(250, 198, 6, 0.4);
+  --hgob20a: rgba(250, 198, 6, 0.2);
+  --hgob00a: rgba(250, 198, 6, 0);
+  --wgo80a: rgba(218, 212, 144, 0.8);
+  --wgo60a: rgba(218, 212, 144, 0.6);
+  --wgo40a: rgba(218, 212, 144, 0.4);
+  --wgo20a: rgba(218, 212, 144, 0.2);
+  --wgo00a: rgba(218, 212, 144, 0);
+  --black80a: rgba(0, 0, 0, 0.8);
+  --black60a: rgba(0, 0, 0, 0.6);
+  --black40a: rgba(0, 0, 0, 0.4);
+  --black20a: rgba(0, 0, 0, 0.2);
+  --black00a: rgba(0, 0, 0, 0);
+  --white80a: rgba(255, 255, 255, 0.8);
+  --white60a: rgba(255, 255, 255, 0.6);
+  --white40a: rgba(255, 255, 255, 0.4);
+  --white20a: rgba(255, 255, 255, 0.2);
+  --white00a: rgba(255, 255, 255, 0);
+  --hero-dropshadow: 0 1px 2px rgba(0, 0, 0, .5);
+  --spotlt: rgb(230, 234, 133);
+  --spotbt: #ffffbb;
+  --all-corners: .25rem .25rem .25rem .25rem;
+  --tab-top-corners: 0.25rem 0.25rem 0 0;
+  --tab-bottom-corners: 0 0 .25rem .25rem;
+}
     h1 {
-        padding: 16px 16px 16px 16px;   
+        padding: 16px 16px 16px 16px;  
+        margin: auto;
+        width : 30%;
+        padding: 10px;
+        text-align: center; 
     }
+
+    .event-card {
+    /* Add shadows to create the "card" effect */
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    }
+
+    /* On mouse-over, add a deeper shadow */
+    .event-card:hover {
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    }
+
+    /* Add some padding inside the card container */
+    .event-container {
+    padding: 2px 16px;
+    margin: 10px;
+    width : 30%;
+    padding: 10px;
+    text-align: center; 
+    }
+
 </style>
     <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,4 +108,13 @@
         <h1>EVENTS PAGE</h1>
         <p> TESTING </p>
 </body>
+
+<div class="event-card">
+  <img src="/assets/favicon.ico" alt="Placeholder" style="width:10%">
+  <div class="event-container">
+    <h4><b>John Doe</b></h4>
+    <p>Architect & Engineer</p>
+  </div>
+</div> 
+
 </html>
