84f16b7062f77d704fafbfd035b9c5c7736b2892
Crowley723 <littlehill723@gmail.com>
2023-11-16 13:52:44 -0800

    added login handling

---
+++ account/handleLogin.php
@@ -0,0 +1,129 @@
+<?php
+$DBhostname = getenv("SQLHOSTNAME");
+$usersDB = getenv("CSC131USERDBNAME");
+$DBusername = getenv("CSC131USERDBUSER");
+$DBpassword = getenv("CSC131USERDBPASS");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    
+    $email = $_POST["email"];
+    $password = $_POST["password1"];
+    
+    if (empty($email)) {
+        echo "Email is required.";
+        exit;
+    }
+    
+    if (empty($password)) {
+        echo "Password is required.";
+        exit;
+    }
+    
+
+    if(!validateEmail($email)){
+        echo "Invalid Email.";
+        exit();
+    }
+    if(!passwordMatchesPattern($password)){
+        echo "Invalid Password.";
+        echo $password;
+        exit();
+    }
+
+    $databaseConnection = mysqli_connect($DBhostname, $DBusername, $DBpassword, $usersDB);
+
+    if (!$databaseConnection) {
+        echo "Error: Unable to connect to MySQL." . PHP_EOL;
+        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }else{
+        echo "<br>Connected to DB </br>";
+    }
+    
+       
+    //prepared sql statement
+    $findUserQuery = mysqli_prepare($databaseConnection, "SELECT * FROM Users WHERE `Email` = ?");
+    mysqli_stmt_bind_param($findUserQuery, "s", $email);
+
+    if ($findUserQuery->execute() === TRUE) {
+        $userResult = mysqli_stmt_get_result($findUserQuery);
+        $userData = mysqli_fetch_assoc($userResult);
+        if ($userData){
+            $hashedPassword = $userData['Hashed Password'];
+            $email = $userData['Email'];
+            $fullName = $userData['Full Name'];
+            if (password_verify($password, $hashedPassword)) {
+                // Store user information in session variables
+                
+                $_SESSION['Email'] = $email;
+                $_SESSION['FullName'] = $fullName;
+            
+                echo "<br>User " . $_SESSION['FullName'] . " Logged In! </br>";
+                // Other user-related information can be stored in session variables as needed
+            } else {
+
+            }
+        }else{
+            exit();
+        }
+        
+    } else {
+        echo "Error: " . $findUserQuery->error;
+        exit();
+    }
+
+    // Close the prepared statement and the database connection
+    mysqli_stmt_close($findUserQuery);
+    $databaseConnection->close();
+}
+function validateEmail($email) {
+    // Define a regular expression for basic email validation
+    $emailRegex = "/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
+
+    // Use the preg_match function to test the email against the regular expression
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return true; // Valid email
+    } else {
+        return false; // Invalid email
+    }
+}
+function validateName($name) {
+    // Define a regular expression for basic email validation
+    $nameRegex = "/^[a-zA-Z' -]+$/";
+    $name = filter_var($name, FILTER_SANITIZE_STRING);
+    // Use the preg_match function to test the email against the regular expression
+    if (preg_match($nameRegex, $name)) {
+        return true; // Valid email
+    } else {
+        return false; // Invalid email
+    }
+}
+function passwordMatchesPattern($password) {
+    // Define a regular expression for basic email validation
+    $passwordRegex = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/";
+
+    // Use the preg_match function to test the email against the regular expression
+    if (preg_match($passwordRegex, $password)) {
+        return true; // Valid email
+    } else {
+        return false; // Invalid email
+    }
+}
+function validateStudentID($studentID) {
+    // Define a regular expression for basic email validation
+    $studentIDRegex = "/^[0-9]{8}$/";
+
+    // Use the preg_match function to test the email against the regular expression
+    if (preg_match($studentIDRegex, $studentID)) {
+        return true; // Valid email
+    } else {
+        return false; // Invalid email
+    }
+}
+
+
+
+
+?>
--- account/login.php
+++ account/login.php
@@ -18,13 +18,13 @@
         <body>
         <div class="signup-box">
             <h2>Log In</h2>
-            <form action="/handleLogin.php" method="post" class="form-container">
+            <form action="/account/handleLogin.php" method="post" class="form-container">
 
                 <div style="display: inline-block"><label for="email" class="entry-label" style="float: left">Email: </label> <div class="existingAccount" style="float: right"><span>Need an account? </span><a href="/account/signup.php">Sign up</a></div></div><br>
                 <input type="email" id="email" name="email" placeholder="Enter Email" required><br><br>
 
                 <label for="password1" class="entry-label">Password: </label><br>
-                <input type="password" id="password1" name="password1" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" placeholder="Enter Password" onkeyup="checkPassword()" required><br><br>
+                <input type="password" id="password1" name="password1" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" placeholder="Enter Password" required><br><br>
 
                 <label class="rememberme">
                     <input type="checkbox" checked="checked" name="remember"> Remember me
