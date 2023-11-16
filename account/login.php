76198f9a1a3641fe4029c6a5652b0e8bf923b72a
Crowley723 <littlehill723@gmail.com>
2023-11-15 17:30:07 -0800

    added login box to login page

--- account/login.php
+++ account/login.php
@@ -4,6 +4,10 @@
     h1 {
         padding: 16px 16px 16px 16px;   
     }
+    .signup-box .rememberme {
+
+    }
+    
 </style>
     <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,7 +15,30 @@
         <?php include("../header.php") ?>
         <link rel="icon" type="image/x-icon" href="/assets/favicon.ico">
     </head>
-    <body>
-        <h1>Log In!</h1>
+        <body>
+        <div class="signup-box">
+            <h2>Log In</h2>
+            <form action="/handleLogin.php" method="post" class="form-container">
+
+                <div style="display: inline-block"><label for="email" class="entry-label" style="float: left">Email: </label> <div class="existingAccount" style="float: right"><span>Need an account? </span><a href="/account/signup.php">Sign up</a></div></div><br>
+                <input type="email" id="email" name="email" placeholder="Enter Email" required><br><br>
+
+                <label for="password1" class="entry-label">Password: </label><br>
+                <input type="password" id="password1" name="password1" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" placeholder="Enter Password" onkeyup="checkPassword()" required><br><br>
+
+                <label class="rememberme">
+                    <input type="checkbox" checked="checked" name="remember"> Remember me
+                </label>
+                
+
+                <button id="submit">Submit</button>
+            </form>
+            <!--div class="separator">
+                <div class="line"></div>
+                <div class="text">OR</div>
+                <div class="line"></div>
+            </div//-->
+        </div>
+    </body>
 </body>
 </html>
