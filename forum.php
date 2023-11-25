ea4a495efb272075de637847d74eb5b2fef74fea
Brynn Crowley <littlehill723@gmail.com>
2023-11-24 20:37:21 -0800

    added quite a few scripts, one gets all posts, updates the page with the posts, handles toggling likes, creating new posts, checking if a user is logged in before they are allowed to post or like

--- forum.php
+++ forum.php
@@ -18,127 +18,21 @@
         <script src="forumScript.js"></script>
     </head>
 
-    <script>
-    jQuery(document).ready(function(){
-        getPosts();
-    });
-
-function getPosts(){
-    $.ajax({
-        url:'/getForumPosts.php',
-        type:'GET',
-        dataType: 'json',
-        success: function(data){
-            updatePage(data);
-        },
-        error: function(xhr, status, error){
-            console.log(status + ': ' + error);
-        }
-    });
-}
-function likePost(){
-    //need to get id of parent post to increment likes.
-
-    $.ajax({
-        url:'/handleLikePost.php',
-        type:'POST',
-        dataType: 'json',
-        success: function(data){
-            updatePage(data);
-        },
-        error: function(xhr, status, error){
-            console.log(status + ': ' + error);
-        }
-    });
-}
-
-function updatePage(data){
-    if(data && data.length > 0){
-        for(var i = data.length - 1; i >= 0; i--){
-            var post = data[i];
-            if(post.hasOwnProperty('AUTHOR') && 
-                post.hasOwnProperty('POST BODY') && 
-                post.hasOwnProperty('LIKES') &&
-                post.hasOwnProperty('TIMESTAMP')){
-
-                const postContainer = document.createElement("div");
-                postContainer.className = 'post-container';
-                postContainer.id = post['ID'];
-                const mainContent = document.getElementById('main-content');
-                mainContent.appendChild(postContainer);
-
-                const userProfile = document.createElement("div");
-                userProfile.className = 'user-profile';
-                postContainer.appendChild(userProfile);
-
-                const profileImage = document.createElement("img");
-                profileImage.src = post['IMAGE PATH']; 
-                userProfile.appendChild(profileImage);
-
-                const profileTextContainer = document.createElement("div");
-                userProfile.appendChild(profileTextContainer);
-
-                const profileName = document.createElement("p");
-                profileName.textContent = post['AUTHOR'];
-                profileTextContainer.appendChild(profileName);
-
-
-                const timestampOptions = {
-                    year: 'numeric',
-                    month: 'long',
-                    day: 'numeric',
-                    hour: 'numeric',
-                    minute: 'numeric',
-                    hour12: true,
-                };
-                const dateObject = new Date(post['TIMESTAMP']);
-
-                const timeStamp = document.createElement("span");
-                timeStamp.textContent = dateObject.toLocaleString('en-US', timestampOptions); 
-                profileTextContainer.appendChild(timeStamp);
-
-                const postText = document.createElement("p");
-                postText.className = 'post-text';
-                postText.textContent = post['POST BODY']; 
-                postContainer.appendChild(postText);
-
-                const postRow = document.createElement("div");
-                postRow.className = 'post-row';
-                postContainer.appendChild(postRow);
-
-
-
-                const activityIcons = document.createElement("div");
-                activityIcons.className = 'activity-icons';
-                postRow.appendChild(activityIcons);
-            
-                const likeButton = document.createElement("div");
-                likeButton.className = 'like-button';
-                activityIcons.appendChild(likeButton);
-                
-                const thumbsUpIcon = document.createElement("button");
-                thumbsUpIcon.id = 'likeBtn2';
-                thumbsUpIcon.className = 'like-btn';
-                const postID = post['ID']
-                thumbsUpIcon.onclick = function(){
-                    toggleLike(postID);
-                }; 
-                thumbsUpIcon.innerText = '👍';
-                likeButton.appendChild(thumbsUpIcon);
-
-                const thumbsUpCounter = document.createElement("span");
-                thumbsUpCounter.className = 'like-count';
-                thumbsUpCounter.innerText = post['LIKES'];
-                likeButton.appendChild(thumbsUpCounter);
-                
-            }
-        }
-    }
-}
-
-</script>
+    
 
 <body>
+    <div id="overlay" class="login-popup-overlay" onclick="closePopup()">
+        <div id="loginPopup" class="login-popup">
+            <div class="login-popup-content">
+                <span class="login-popup-close" onclick="closePopup()">&times;</span>
+
+                <p><a href="/account/login.php">Log In</a> to like or create posts.</p>
+            <!-- Add a login button or link here -->
+            </div>
+        </div>
+    </div>
+    
+
     <!--<h1>Community Fourm</h1>-->
     <nav>
         <div class="nav-left">
@@ -153,55 +47,18 @@ function updatePage(data){
     </div>
     <!-------- main-content -------- --> 
     <div class="main-content" id="main-content">
-        <div class="post-container">
-        <div class="user-profile">
-                <img src="images/profile-pic.png">
-                <div>
-                    <p>Carol Jones</p>
-                    <span>October 19 2023, 3:23 pm</span>
-                </div>
-            </div>
-            <p class="post-text">Making websites suck</p>
-            <div class="post-row">
-                <div class="activity-icons">
-                <!-- Thumbs Up Button -->
-                <div class="like-button">
-                    <button onclick="toggleLike(1)" id="likeBtn1" class="like-btn">👍</button>
-                    <span id="likeCount1" class="like-count">0</span>
-                </div>
-                </div>
-            </div>
-        </div>
-        <div class="post-container">
-        <div class="user-profile">
-                <img src="images/member-1.png">
-                <div>
-                    <p>Scarlett</p>
-                    <span>june 06 2023, 6:66 pm</span>
-                </div>
-            </div>
-            <p class="post-text">Sac State has the worst cs department</p>
-            <div class="post-row">
-                <div class="activity-icons">
-                <!-- Thumbs Up Button -->
-                    <div class="like-button">
-                        <button onclick="toggleLike(2)" id="likeBtn2" class="like-btn">👍</button>
-                        <span id="likeCount2" class="like-count">0</span>
-                    </div>
-                </div>
-            </div>
-        </div>
+
     </div>
     <!-------- right-sidebar -------- --> 
     <div class="right-sidebar">
         <!-- Post Button -->
-        <button class="post-button" onclick="openModal()">Post</button>
+        <button class="post-button" onclick="checkLoginStatusPost()">Post</button>
 
         <!-- Modal -->
         <div id="postModal" class="modal">
             <div class="modal-content">
                 <h2>Share Your Thoughts</h2>
-                <form action="/addPost.php" method="POST">
+                <form action="/createForumPost.php" method="POST">
                     <textarea id="postText" name="postText" placeholder="Type your post here" required></textarea>
                     <input type="submit">
                     <button type="button" onclick="closeModal()" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
--- forumScript.js
+++ forumScript.js
@@ -15,26 +15,244 @@ function openModal() {
     console.log('Submitted Post:', postContent);
     closeModal(); // Close the modal after submission (You might want to modify this behavior)
   }
-// Function to handle the like functionality
-function toggleLike(buttonId) {
-    const likeBtn = document.getElementById(`likeBtn${buttonId}`);
-    const likeCount = document.getElementById(`likeCount${buttonId}`);
-  
-    // Get the current like count
-    let currentCount = parseInt(likeCount.innerText);
-  
-    // Toggle the like status
-    if (likeBtn.innerText === '👍') {
-      likeBtn.innerText = '👎';
-      likeBtn.style.backgroundColor = '#efefef'; // Change color when liked
-      currentCount++;
-    } else {
-      likeBtn.innerText = '👍';
-      likeBtn.style.backgroundColor = '#efefef'; // Change color back when unliked
-      currentCount--;
+
+    jQuery(document).ready(function(){
+        getPosts();
+    });
+
+function getPosts(){
+    $.ajax({
+        url:'/getForumPosts.php',
+        type:'GET',
+        dataType: 'json',
+        success: function(data){
+            updatePage(data);
+        },
+        error: function(xhr, status, error){
+            console.log(status + ': ' + error);
+        }
+    });
+}
+
+function updatePage(dataUnsorted){
+    dataUnsorted.sort(function(a, b) {
+    // Change '<' to '>' for descending order
+    return a.TIMESTAMP.localeCompare(b.TIMESTAMP);
+});
+data = dataUnsorted;
+console.log(data);
+    if(data && data.length > 0){
+        for(var i = data.length - 1; i >= 0; i--){
+            var post = data[i];
+            if( post.hasOwnProperty('FULL NAME') &&
+                post.hasOwnProperty('POST BODY') && 
+                post.hasOwnProperty('LIKES') &&
+                post.hasOwnProperty('TIMESTAMP')){
+
+
+                const postContainer = document.createElement("div");
+                postContainer.className = 'post-container';
+                postContainer.id = post['ID'];
+
+                const mainContent = document.getElementById('main-content');
+                mainContent.appendChild(postContainer);
+
+                const userProfile = document.createElement("div");
+                userProfile.className = 'user-profile';
+                postContainer.appendChild(userProfile);
+
+                
+                var identicon = new Image();
+                identicon.src = "data:image/png;base64," + post['ICON'];
+                userProfile.appendChild(identicon);
+
+                const profileTextContainer = document.createElement("div");
+                userProfile.appendChild(profileTextContainer);
+
+                const profileName = document.createElement("p");
+                profileName.textContent = post['FULL NAME'];
+                profileTextContainer.appendChild(profileName);
+
+
+                const timestampOptions = {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    hour12: true,
+                };
+                const dateObject = new Date(post['TIMESTAMP']);
+
+                const timeStamp = document.createElement("span");
+                timeStamp.textContent = dateObject.toLocaleString('en-US', timestampOptions); 
+                profileTextContainer.appendChild(timeStamp);
+
+                const postText = document.createElement("p");
+                postText.className = 'post-text';
+                postText.textContent = post['POST BODY']; 
+                postContainer.appendChild(postText);
+
+                const postRow = document.createElement("div");
+                postRow.className = 'post-row';
+                postContainer.appendChild(postRow);
+
+
+
+                const activityIcons = document.createElement("div");
+                activityIcons.className = 'activity-icons';
+                postRow.appendChild(activityIcons);
+            
+                const likeButton = document.createElement("div");
+                likeButton.className = 'like-button';
+                activityIcons.appendChild(likeButton);
+                
+                const thumbsUpIcon = document.createElement("button");
+                thumbsUpIcon.className = 'like-btn';
+                const postID = post['ID']
+                thumbsUpIcon.onclick = function(){
+                    checkLoginStatusLike(this.parentNode.parentNode.parentNode.parentNode.id);
+                    
+                }; 
+                if(post['USER LIKED'] == 1){
+                    thumbsUpIcon.innerText = '👎';
+                }else{
+                    thumbsUpIcon.innerText = '👍';
+                }
+                
+                likeButton.appendChild(thumbsUpIcon);
+
+                const thumbsUpCounter = document.createElement("span");
+                thumbsUpCounter.className = 'like-count';
+                thumbsUpCounter.innerText = post['LIKES'];
+                likeButton.appendChild(thumbsUpCounter);
+                
+            }else{
+                console.log("Missing Fields for Posts");
+            }
+        }
     }
-  
-    // Update the like count
-    likeCount.innerText = currentCount;
-  }
-  
+}
+function showLoginPopup() {
+    showOverlay();
+    var popup = document.getElementById("loginPopup");
+    popup.style.display = "block";
+}
+
+// Function to close the login pop-up
+function closePopup() {
+    var popup = document.getElementById("loginPopup");
+    popup.style.display = "none";
+    hideOverlay();
+}
+function showOverlay() {
+    $("#overlay").show();
+}
+function hideOverlay() {
+    $("#overlay").hide();
+}
+
+
+
+function checkLoginStatusPost(){
+    $.ajax({
+      type: "POST",
+      url: "/account/checkLoginStatus.php",
+      success: function (response) {
+        if (response === "authenticated") {
+            openModal()
+            return true;
+        } else {
+          showLoginPopup();
+          return false;
+        }
+      },
+      error: function (error) {
+        console.log("Error checking login status: " + error);
+      }
+    });
+}
+function checkLoginStatusLike(postID){
+    $.ajax({
+      type: "POST",
+      url: "/account/checkLoginStatus.php",
+      success: function (response) {
+        if (response === "authenticated") {
+            
+            toggleLike(postID);
+            return true;
+        } else {
+         showLoginPopup();
+          return false;
+        }
+      },
+      error: function (error) {
+        console.log("Error checking login status: " + error);
+      }
+    });
+}
+function checkLoginStatus(){
+    $.ajax({
+      type: "POST",
+      url: "/account/checkLoginStatus.php",
+      success: function (response) {
+        if (response === "authenticated") {
+            return true;
+        } else {
+          return false;
+        }
+      },
+      error: function (error) {
+        console.log("Error checking login status: " + error);
+      }
+    });
+}
+function toggleLike(parentID){
+    $.ajax({
+        type: "POST",
+        url: "/handleToggleLikePost.php",
+        data: jQuery.param({ PostID: parentID }),
+        contentType: "application/x-www-form-urlencoded",
+        success: function (response) {
+            // Check if the toggle was successful
+            updatePost(parentID);
+            
+        },
+        error: function (error) {
+            console.log("Error toggling like: " + error);
+        }
+    });
+}
+function updatePost(parentID){
+    $.ajax({
+        url: '/getSingleForumPost.php', // Adjust the URL to fetch a single post
+        type: 'GET',
+        data: jQuery.param({ PostID: parentID }),
+        contentType: "application/x-www-form-urlencoded",
+        success: function (postData) {
+            // Find and update the existing post on the page
+            var postContainer = document.getElementById(parentID);
+            if (postContainer) {
+                // Update the likes count and button based on the response
+                var likeButton = postContainer.querySelector('.like-button');
+                var thumbsUpCounter = likeButton.querySelector('.like-count');
+                var thumbsUpIcon = likeButton.querySelector('.like-btn');
+    
+                thumbsUpCounter.innerText = postData['LIKES'];
+                console.log(postData);
+
+                if (postData['USER LIKED'] == 1) {
+                    thumbsUpIcon.innerText = '👎';
+                } else {
+                    thumbsUpIcon.innerText = '👍';
+                }
+            } else {
+                console.log("Post container not found");
+            }
+        },
+        error: function (error) {
+            console.log("Error loading single post:" + error);
+        }
+    });
+}
+
