5b4215d725222694e489f79de625268f6b689f2f
Danny Le <dannyle@csus.edu>
2023-11-29 19:08:58 -0800

    Footer first push

--- footer.php
+++ footer.php
@@ -1,6 +1,26 @@
 <!DOCTYPE html>
-<html>
-    <body>
-aasdadsawdad
-    </body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>footer</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <footer>
+        <div class="footer-content">
+            <h3>Connect with us!</h3>
+            <ul class="socials">
+                <li><a href="https://www.facebook.com/sacstate/"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="https://twitter.com/sacstate?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="https://www.instagram.com/sacstate/?hl=en"><i class="fa fa-instagram"></i></a></li>
+                <li><a href="https://www.linkedin.com/school/sacstate/"><i class="fa fa-linkedin-square"></i></a></li>
+            </ul>
+        </div>
+        <div class="footer-bottom">
+            <p>California State University, Sacramento | 6000 J Street Sacramento, CA 95819 | (916) 278-6011</p>
+        </div>
+    </footer>
+</body>
 </html>
