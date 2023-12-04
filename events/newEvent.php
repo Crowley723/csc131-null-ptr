fa20af23b0327d2f0ea53589aae3c911fa494302
Brynn Crowley <littlehill723@gmail.com>
2023-12-04 02:51:25 -0800

    fix styling for cost box, change field names to be more descriptive

--- events/newEvent.php
+++ events/newEvent.php
@@ -10,25 +10,25 @@
     <h1 class="heading">New Event</h1>
     <form class="form" action="/events/handleNewEvent.php" method="post" enctype="multipart/form-data">
       <label class="form-label" for="title">Title:</label>
-      <input class="form-input" type="text" id="title" name="title" placeholder="Enter the title" required>
+      <input class="form-input" type="text" id="EventTitle" placeholder="Enter the title" required>
 
       <label class="form-label" for="cost">Cost:</label>
         <div class="cost-input">
-            <input type="number" id="cost" name="cost" min="0" value="0">
+            <input type="number" id="EventCost" min="0" value="0">
             
         </div>
       
       <label class="form-label" for="location">Location:</label>
-      <input class="form-input" type="text" id="location" name="location" placeholder="Enter the location" required>
+      <input class="form-input" type="text" id="EventLocation" placeholder="Enter the location" required>
 
       <label class="form-label" for="description">Description:</label>
-      <textarea class="form-textarea" id="description" name="description" placeholder="Enter the description" required></textarea>
+      <textarea class="form-textarea" id="EventDescription" placeholder="Enter the description" required></textarea>
 
       <label class="form-label" for="link">Link:</label>
-      <input class="form-input" type="text" id="link" name="link" placeholder="Enter the link" required>
+      <input class="form-input" type="text" id="EventLink" placeholder="Enter the link" required>
 
       <label class="form-label" for="image">Image:</label>
-      <input class="form-file" type="file" id="image" name="image" accept="image/*" required>
+      <input class="form-file" type="file" id="EventImage" accept="image/*" required>
 
       <input class="submit-btn" type="submit" value="Submit">
     </form>
@@ -36,7 +36,7 @@
 </body>
 <script>
     function changeCost(value) {
-  var costInput = document.getElementById("cost");
+  var costInput = document.getElementById("EventCost");
   var currentValue = parseInt(costInput.value) || 0;
   var newValue = currentValue + value;
   if (newValue >= 0) {
